acf8ce1c8fbfb347832a14c16fe561d1f370aced
Mark <kalmark@mail.ru>
2022-10-17 20:38:02 +0300

    Add Ex_26_NumDigit

---
+++ Ex_26_NumDigit/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 26: 
+// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int NumDigit (int num)
+{
+    int count = 0;
+    if (num == 0) count = 1;
+    else
+    {
+        while (num > 0)
+        {
+            num = num / 10;
+            count += 1;
+        }
+    }
+    return count;
+}
+
+int count = NumDigit(num);
+Console.WriteLine($"В этом числе {count} цифр");
