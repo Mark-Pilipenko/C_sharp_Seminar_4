0891bc51afbc9f58394721e5c432404442dbe6f7
Mark <kalmark@mail.ru>
2022-10-17 21:55:52 +0300

    Add Ex_30_RandomArray

---
+++ Ex_30_RandomArray/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 30: 
+// Напишите программу, которая выводит массив из 8 элементов, 
+// заполненный нулями и единицами в случайном порядке.
+// 1,0,1,1,0,1,0,0
+
+Console.Write("Введите длину массива: ");
+int len = Convert.ToInt32(Console.ReadLine());
+int[] rand = new int[len];
+
+rand = FillArray(len, rand);
+PrintArray(len, rand);
+
+int[] FillArray(int n, int[] array)
+{
+for (int i = 0; i < n; i++)
+{
+    array[i] = new Random().Next(0, 2);
+}
+return array;
+}
+
+void PrintArray(int n, int[] array)
+{
+for (int i = 0; i < n; i++)
+{
+    if (i == n-1) Console.Write($"{array[i]}");
+    else Console.Write($"{array[i]}, ");
+}
+}
